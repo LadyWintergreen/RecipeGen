62b0b058d4e53c57747243a1c98b051598f4f094
Elizabeth Perry <mper0066@student.monash.edu>
2023-05-08 17:44:50 +1000

    rewrote entire seq2seq

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import glob
 import re
 
 EOS_token = "EOS"
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class Language:
     def __init__(self, name):
@@ -27,7 +28,25 @@ class Language:
         else:
             self.word2count[word] += 1
 
+def build_language(data_path):
+    all_ingredients = []
+    all_recipes = []
+    for path in data_path: #manual path list:
+        recipes = process_text_data(path)
+        for r in recipes:
+            all_ingredients.append(r[0])
+            all_recipes.append(r[1])
+    ingredient_lang = Language("ingredients")
+    recipe_lang = Language("recipes")
+    for ing in all_ingredients:
+        ingredient_lang.addSentence(ing)
+    for rec in all_recipes:
+        recipe_lang.addSentence(rec)
+    return ingredient_lang, recipe_lang
+        
+
 def process_text_data(path):
+    print("Processing text data from {}".format(path))
     recipes = []
     files = glob.glob(path + "/*.txt")
     for file in files:
@@ -36,19 +55,17 @@ def process_text_data(path):
             recipe = text_to_recipe_processing(l)
             if recipe is not None:
                 recipes.append(recipe)
-    return recipes
+    return recipes #TODO: build language here as well!!
 
 def indexesFromSentence(lang, sentence):
     return [lang.word2index[word] for word in sentence.split(' ')]
 
-
 def tensorFromSentence(lang, sentence):
     indexes = indexesFromSentence(lang, sentence)
     indexes.append(EOS_token)
-    return torch.tensor(indexes, dtype=torch.long, device=device).view(-1, 1)
+    return torch.tensor(indexes, dtype=torch.long, device=DEVICE).view(-1, 1)
 
-
-def tensorsFromPair(pair):
+def tensorsFromPair(pair, input_lang, output_lang):
     input_tensor = tensorFromSentence(input_lang, pair[0])
     target_tensor = tensorFromSentence(output_lang, pair[1])
     return (input_tensor, target_tensor)
@@ -68,17 +85,41 @@ def text_to_recipe_processing(line):
 test = process_text_data("Cooking_Dataset/test")
 print(test[0])
 
+def collate_fn(data): # may be able to just pack instead
+    def merge(sequences):
+        lengths = [len(seq) for seq in sequences]
+        padded_seqs = torch.zeros(len(sequences), max(lengths)).long()
+        for i, seq in enumerate(sequences):
+            end = lengths[i]
+            padded_seqs[i, :end] = seq[:end]
+        return padded_seqs, lengths
+    #Goal is to create minibatch tensors from single batch data
+    # sort a list by sequence length (descending order) to use pack_padded_sequence
+    data.sort(key=lambda x: len(x[0]), reverse=True)
+    # seperate source and target sequences
+    src_seqs, trg_seqs = zip(*data)
+    # merge sequences (from tuple of 1D tensor to 2D tensor)
+    src_seqs, src_lengths = merge(src_seqs)
+    trg_seqs, trg_lengths = merge(trg_seqs)
+
+    return src_seqs, src_lengths, trg_seqs, trg_lengths
+
+
 class RecipeData(Dataset):
-    def __init__(self, path):
+    def __init__(self, path, ing_lang, instr_lang):
         self.all_recipes = process_text_data(path)
+        self.ing_lang = ing_lang
+        self.instr_lang = instr_lang
 
     def __len__(self):
         return len(self.all_recipes)
     
     def __getitem__(self, ndx):
         item = self.all_recipes[ndx]
-        ingredients, steps = tensorsFromPair(item)
+        ingredients, steps = tensorsFromPair(item, self.ing_lang, self.instr_lang)
         return ingredients, steps
+
+
 
 #TODO:
 # 1. data processing/preprocessing
